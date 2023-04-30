c651a37f961eb64c4bdfa4bc88fe4eb56cdd4335
ArchiGrumpy <it.arthur.g@gmail.com>
2023-04-30 18:52:58 +0300

    Create FunctionCLR.cs

---
+++ HW21 - CLR/RegexpProject/RegexpProject/FunctionCLR.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Text.RegularExpressions;
+using System.Data;
+
+namespace RegexpProject
+{
+    public static class FunctionsCLR
+    {
+        [SqlFunction(IsDeterministic = true)]
+        public static SqlBoolean IsMatch(SqlString str, SqlString pattern)
+        {
+            var reg = new Regex(pattern.ToString());
+            return (SqlBoolean)reg.IsMatch(str.ToString());
+        }
+
+    }
+}
